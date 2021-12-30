4f0693f72b274df157fe504ab7706ace0dccfb64
Алексей <gogh78@mail.ru>
2021-12-30 19:36:25 +0400

    Добавьте файлы проекта.

---
+++ Exercise17/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise17
+{
+    class BankAccount<T>
+    //Создать класс для моделирования счета в банке.
+    //Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.
+    //Класс должен быть объявлен как обобщенный.
+    //Универсальный параметр T должен определять тип номера счета.
+    {
+        public T Num { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public BankAccount(T num, string name, int balance)
+        {
+            Num = num;
+            Name = name;
+            Balance = balance;
+        }
+        public string GetInfo()
+        {
+            return $"{Num}, {Name}, {Balance}";
+        }
+    }
+}
---
+++ Exercise17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise17
+
+    //Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения.
+    //Создать  несколько экземпляров класса, параметризированного различными типам.
+    //Заполнить его поля и вывести информацию об экземпляре класса на печать.
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите числовой номер счета");
+                int numInt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите ФИО владельца");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите баланс");
+                int balance = Convert.ToInt32(Console.ReadLine());
+
+                BankAccount<int> bankAccount1 = new BankAccount<int>(numInt, name, balance);
+
+                Console.WriteLine("Введите строковый номер счета");
+                string numString = Console.ReadLine();
+                Console.WriteLine("Введите имя");
+                name = Console.ReadLine();
+                Console.WriteLine("Введите баланс");
+                balance = Convert.ToInt32(Console.ReadLine());
+
+                BankAccount<string> bankAccount2 = new BankAccount<string>(numString, name, balance);
+
+                Console.WriteLine(bankAccount1.GetInfo());
+                Console.WriteLine(bankAccount2.GetInfo());
+                Console.ReadKey();
+            }
+        }
+    }
